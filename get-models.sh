61321326dd9a39983cb926a032e238a484951f46
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2018-02-22 17:30:22 +0700

    fix(get-models): change to mkdir if not exist

--- get-models.sh
+++ get-models.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-mkdir -r models
+mkdir -p models
 wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2 -P models
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 -P models
 
