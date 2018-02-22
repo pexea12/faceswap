6539d8159fea21f5d3e16887491b75a9cc9452a0
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2018-02-22 17:22:21 +0700

    feat(get-models): add get models scripts

---
+++ get-models.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash 
+
+mkdir -r models
+wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2 -P models
+wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 -P models
+
+cd models 
+bzip2 -d dlib_face_recognition_resnet_model_v1.dat.bz2
+bzip2 -d shape_predictor_68_face_landmarks.dat.bz2 
+
+rm -rf dlib_face_recognition_resnet_model_v1.dat.bz2
+rm -rf shape_predictor_68_face_landmarks.dat.bz2 
