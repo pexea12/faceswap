698bc2fa1dcca1456138c44e050d3d1e94b83ded
Nguyen Thien Dung <dungtoanhoc@yahoo.com.vn>
2018-02-22 17:33:56 +0700

    fix(get-models): fix cd command

--- get-models.sh
+++ get-models.sh
@@ -4,9 +4,8 @@ mkdir -p models
 wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2 -P models
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 -P models
 
-cd models 
-bzip2 -d dlib_face_recognition_resnet_model_v1.dat.bz2
-bzip2 -d shape_predictor_68_face_landmarks.dat.bz2 
+bzip2 -d models/dlib_face_recognition_resnet_model_v1.dat.bz2
+bzip2 -d models/shape_predictor_68_face_landmarks.dat.bz2 
 
-rm -rf dlib_face_recognition_resnet_model_v1.dat.bz2
-rm -rf shape_predictor_68_face_landmarks.dat.bz2 
+rm -rf models/dlib_face_recognition_resnet_model_v1.dat.bz2
+rm -rf models/shape_predictor_68_face_landmarks.dat.bz2 
